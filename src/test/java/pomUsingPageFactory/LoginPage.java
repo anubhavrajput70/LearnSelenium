e53fc0ce6e46eb505082d27baa697559e6ee0c58
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-03-27 01:42:22 +0530

    updation

--- src/test/java/pomUsingPageFactory/LoginPage.java
+++ src/test/java/pomUsingPageFactory/LoginPage.java
@@ -10,38 +10,37 @@ import org.openqa.selenium.support.PageFactory;
 //locator
 //action method
 public class LoginPage {
-	
+
 	WebDriver driver;
-	
-	//constructor
-	public LoginPage(WebDriver driver)
-	{
-		this.driver=driver;
-		//this will try to initiate all the elements using the driver which we passed
-		PageFactory.initElements(driver,this);
+
+	// constructor
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		// this will try to initiate all the elements using the driver which we passed
+		PageFactory.initElements(driver, this);
 	}
-	
-	//locators
-	 
-	//@FindBy(xpath = "//input[@placeholder='Username']") WebElement text_userName;
-	//another way to declare webelements
-	@FindBy(how=How.XPATH,using = "//input[@placeholder='Username']") WebElement text_userName;
-	@FindBy(xpath = "//input[@placeholder='Password']") WebElement text_password;
-	@FindBy(xpath = "(//button[normalize-space()='Login'])[1]") WebElement login_btn;
-	
-	//Actions
-	public void setUserName(String userName)
-	{
+
+	// locators
+
+	// @FindBy(xpath = "//input[@placeholder='Username']") WebElement text_userName;
+	// another way to declare webelements
+	@FindBy(how = How.XPATH, using = "//input[@placeholder='Username']")
+	WebElement text_userName;
+	@FindBy(xpath = "//input[@placeholder='Password']")
+	WebElement text_password;
+	@FindBy(xpath = "(//button[normalize-space()='Login'])[1]")
+	WebElement login_btn;
+
+	// Actions
+	public void setUserName(String userName) {
 		text_userName.sendKeys(userName);
 	}
-	
-	public void setPassword(String password)
-	{
+
+	public void setPassword(String password) {
 		text_password.sendKeys(password);
 	}
 
-	public void clickLogin()
-	{
+	public void clickLogin() {
 		login_btn.click();
 	}
 }
