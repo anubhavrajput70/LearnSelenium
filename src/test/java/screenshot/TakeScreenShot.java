90aa9256526f85d9790a1e104f7de14f45a55b96
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-05-25 23:12:59 +0530

    handle Alert using js executor

--- src/test/java/screenshot/TakeScreenShot.java
+++ src/test/java/screenshot/TakeScreenShot.java
@@ -13,37 +13,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TakeScreenShot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demo.nopcommerce.com");
 		driver.manage().window().maximize();
-		
+
 //		//capture full page screenshot
 //		TakesScreenshot ts= (TakesScreenshot) driver;
 //		File sourceFile = ts.getScreenshotAs(OutputType.FILE);
 //		File targetFile = new File(System.getProperty("user.dir")+"\\screenShot\\fullpage.png");
 //		//copy source file to target file
 //		sourceFile.renameTo(targetFile);
-		
-		//capture screenshot of specific area of the page
+
+		// capture screenshot of specific area of the page
 //		WebElement box=driver.findElement(By.xpath("//div[@class='product-grid home-page-product-grid']"));
 //		File sourceFile = box.getScreenshotAs(OutputType.FILE);
 //		File targetFile = new File(System.getProperty("user.dir")+"\\screenShot\\fullpage1.png");
 //		//copy source file to target file
 //		sourceFile.renameTo(targetFile);
-		
-		//capture screenshot of webElement
-		WebElement box=driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+
+		// capture screenshot of webElement
+		WebElement box = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
 		File sourceFile = box.getScreenshotAs(OutputType.FILE);
-		File targetFile = new File(System.getProperty("user.dir")+"\\screenShot\\fullpage2.png");
-		//copy source file to target file
+		File targetFile = new File(System.getProperty("user.dir") + "\\screenShot\\fullpage2.png");
+		// copy source file to target file
 		sourceFile.renameTo(targetFile);
-		
-	
-		
-		
-		
 
 	}
 
