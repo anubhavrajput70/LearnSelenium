59f9029827121920e6f9fba1aae174ec752c8fc9
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-05-26 01:17:49 +0530

    check if dropdown is sorted

---
+++ src/test/java/dropDown/CheckIfDropDownIsSorted.java
@@ -0,0 +1,48 @@
+package dropDown;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CheckIfDropDownIsSorted {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+	
+		Select dropdown = new Select(driver.findElement(By.xpath("//select[@id='colors']")));
+		
+		ArrayList original = new ArrayList();
+		ArrayList temp = new ArrayList();
+		
+		List<WebElement> options = dropdown.getOptions();
+		
+		for(WebElement option:options)
+		{
+			original.add(option.getText());
+			temp.add(option.getText());
+		}
+		Collections.sort(temp);
+		if(!original.equals(temp))
+		{
+			System.out.println("list was not sorted");
+			System.out.println(original);
+			System.out.println(temp);
+		}
+		else
+		{
+			System.out.println("list was sorted");
+		}
+	}
+
+}
